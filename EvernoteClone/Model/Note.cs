a4af996542787a17619e375051889289fe36f360
Noah Day <sheltielover432@gmail.com>
2021-05-11 21:03:06 -0400

    Model Setup

---
+++ EvernoteClone/Model/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvernoteClone.Model
+{
+    internal class Note
+    {
+        public int ID { get; set; }
+        public int NotebookID { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
